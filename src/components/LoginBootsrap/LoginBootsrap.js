0d48b2ea62e470ef768943d0669f40c49ebd66d7
Md Aslam Ali <mdaslamali675@gmail.com>
2022-10-15 14:31:50 +0600

    image add

--- src/components/LoginBootsrap/LoginBootsrap.js
+++ src/components/LoginBootsrap/LoginBootsrap.js
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
 import app from '../../Firebase/firebase.init';
+import login from "../../assests/login1.png"
 
 const auth = getAuth(app)
 
@@ -47,25 +48,29 @@ const LoginBootsrap = () => {
     }
 
     return (
-            <div className='w-50 mx-auto'>
-                <h2>Log in your account</h2>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control onBlur={handleEmailBlur} type="email" name="email" placeholder="Enter email" />
-                </Form.Group>
+            <div className='d-flex justify-content-center align-items-center'>
+            <div className='w-50'>
+                <img className='w-100' src={login} alt="" />
+            </div>
+            <div className='w-50'>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control onBlur={handleEmailBlur} type="email" name="email" placeholder="Enter email" />
+                    </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name="password" placeholder="Password" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-                {success && <p className='text-success'>Successfully Login!!!!!</p>}
-                <p>new To the website!! Please <Link to="/register">Register</Link></p>
-                <p>Forget password <button onClick={handleForgetPassword} className='btn text-primary btn-underline'>reset</button> </p>
-            </Form>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" name="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                    {success && <p className='text-success'>Successfully Login!!!!!</p>}
+                    <p>new To the website!! Please <Link to="/register">Register</Link></p>
+                    <p>Forget password <button onClick={handleForgetPassword} className='btn text-primary btn-underline'>reset</button> </p>
+                </Form>
+            </div>
             </div>
     );
 };
